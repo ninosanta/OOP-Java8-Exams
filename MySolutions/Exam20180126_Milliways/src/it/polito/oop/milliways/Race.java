454224c6c97b4814b604c8c5bb4b0e8abbfc5e4e
ninosanta <38584348+ninosanta@users.noreply.github.com>
2018-06-12 12:39:39 +0200

    R1

--- MySolutions/Exam20180126_Milliways/src/it/polito/oop/milliways/Race.java
+++ MySolutions/Exam20180126_Milliways/src/it/polito/oop/milliways/Race.java
@@ -1,17 +1,34 @@
 package it.polito.oop.milliways;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static java.util.stream.Collectors.*;
 
 public class Race {
-    
+	
+	private String name;
+	Set<String> requirements = new HashSet<>();
+	
+	
+	public Race(String name) {
+		this.name = name;
+	}
+	
 	public void addRequirement(String requirement) throws MilliwaysException {
+		if(requirements.contains(requirement))
+			throw new MilliwaysException();
+		else
+			requirements.add(requirement);
 	}
 	
 	public List<String> getRequirements() {
-        return null;
+        return requirements.stream().collect(toList());
 	}
 	
 	public String getName() {
-        return null;
+        return name;
 	}
+	
 }
