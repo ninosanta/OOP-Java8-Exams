4e4b7374e12c3c5c7f93f49e41d64d22bda9c435
ninosanta <38584348+ninosanta@users.noreply.github.com>
2018-06-12 12:39:17 +0200

    R2

--- MySolutions/Exam20180126_Milliways/src/it/polito/oop/milliways/Party.java
+++ MySolutions/Exam20180126_Milliways/src/it/polito/oop/milliways/Party.java
@@ -1,27 +1,44 @@
 package it.polito.oop.milliways;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeSet;
+
+import static java.util.stream.Collectors.*;
 
 public class Party {
-
+	
+	Map<Race, Integer> groups = new HashMap<>(); 
+	Map<String, Integer> description = new HashMap<>();
+	int num;
+	
+	
     public void addCompanions(Race race, int num) {
+    	groups.put(race, num);
+    	description.put(race.getName(), num);
+    	this.num += num;
 	}
 
 	public int getNum() {
-        return -1;
+        return num;
 	}
 
 	public int getNum(Race race) {
-	    return -1;
+	    return groups.get(race);
 	}
 
 	public List<String> getRequirements() {
-        return null;
-	}
+		return groups.keySet().stream()
+				.map(Race::getRequirements)  // Stream<List<String>>
+				.flatMap(List::stream)  // Stream<String>
+				.sorted((s1,s2) -> s1.compareTo(s2))
+				.distinct()
+				.collect(toList());
+	}	
 
-    public Map<String,Integer> getDescription(){
-        return null;
+    public Map<String,Integer> getDescription() {
+        return description;        			 
     }
 
 }
