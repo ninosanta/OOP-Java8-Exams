f6a100ef4bab12480a0a59b1f1a762e17accb1db
ninosanta <38584348+ninosanta@users.noreply.github.com>
2018-06-13 17:13:11 +0200

    until R4

--- MySolutions/Exam20180126_Milliways/src/it/polito/oop/milliways/Restaurant.java
+++ MySolutions/Exam20180126_Milliways/src/it/polito/oop/milliways/Restaurant.java
@@ -1,8 +1,10 @@
 package it.polito.oop.milliways;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 
 public class Restaurant {
@@ -10,6 +12,7 @@ public class Restaurant {
 	Map<String, Race> races = new HashMap<>();
 //	Map<String, Party> parties = new HashMap<>();
 	Map<Integer, Hall> halls = new HashMap<>();
+	List<Hall> hallsList = new LinkedList<>();
 	
     public Restaurant() {
 	}
@@ -32,19 +35,29 @@ public class Restaurant {
 	    	throw new MilliwaysException();
 	    	
 		halls.put(id, new Hall(id));
+		hallsList.add(halls.get(id));
 	    return halls.get(id);
 	}
 
 	public List<Hall> getHallList() {
-		return null;
+		return hallsList;
 	}
 
 	public Hall seat(Party party, Hall hall) throws MilliwaysException {
-        return null;
+		if (hall.isSuitable(party) == false)
+			throw new MilliwaysException();
+		
+        return hall;
 	}
 
 	public Hall seat(Party party) throws MilliwaysException {
-        return null;
+        Optional<Hall> hall = halls.values().stream()
+        		.filter(h -> h.isSuitable(party))
+        		.findFirst();
+        if (hall.isPresent())
+        	return hall.get();
+        else 
+        	throw new MilliwaysException();
 	}
 
 	public Map<Race, Integer> statComposition() {
