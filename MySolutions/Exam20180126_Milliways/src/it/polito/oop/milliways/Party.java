c2ab987c79112b08ab2889fafda9fcf23f2774f0
ninosanta <38584348+ninosanta@users.noreply.github.com>
2018-06-13 19:49:47 +0200

    until R5.1

--- MySolutions/Exam20180126_Milliways/src/it/polito/oop/milliways/Party.java
+++ MySolutions/Exam20180126_Milliways/src/it/polito/oop/milliways/Party.java
@@ -8,29 +8,33 @@ import static java.util.stream.Collectors.*;
 
 public class Party {
 	
-	Map<Race, Integer> groups = new HashMap<>(); 
+	Map<Race, Integer> companions = new HashMap<>(); 
 	Map<String, Integer> description = new HashMap<>();
 	int num;
 	
 	
     public void addCompanions(Race race, int num) {
     	this.num += num;
-    	if (groups.containsKey(race))
-    		num += groups.get(race);
-    	groups.put(race, num);
+    	if (companions.containsKey(race))
+    		num += companions.get(race);
+    	companions.put(race, num);
     	description.put(race.getName(), num);
 	}
+    
+    public Map<Race, Integer> getCompanions() {
+    	return companions;
+    }
 
 	public int getNum() {
         return num;
 	}
 
 	public int getNum(Race race) {
-	    return groups.get(race);
+	    return companions.get(race);
 	}
 
 	public List<String> getRequirements() {
-		return groups.keySet().stream()
+		return companions.keySet().stream()
 				.map(Race::getRequirements)  // Stream<List<String>>
 				.flatMap(List::stream)  // Stream<String>
 				.sorted((s1,s2) -> s1.compareTo(s2))
